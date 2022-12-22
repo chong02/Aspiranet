f68519ca6ffe24669bf76356b293ed4e6d0c5153
Caleb Hong <chong02@berkeley.edu>
2022-12-21 16:43:02 -0800

    remove extra libraries

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,6 @@
 # Import necessary libraries
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 from datetime import *
 import geopandas as gpd
 import folium
